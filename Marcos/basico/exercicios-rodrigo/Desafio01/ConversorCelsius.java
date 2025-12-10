71a573c1c44420209572e0178291099dd525b28c
Yuri <marcos.yuriro@gmail.com>
2025-12-10 14:23:45 -0300

    Exercícios Rodrigo: 01 e 02

---
+++ Marcos/basico/exercicios-rodrigo/Desafio01/ConversorCelsius.java
@@ -0,0 +1,22 @@
+public class ConversorCelsius {
+    private int celsius;
+
+    public ConversorCelsius(int celsius) {
+        this.celsius = celsius;
+    }
+
+    public double converterFahrenheit() {
+        double fahrenheit = celsius * 1.8 + 32;
+        return fahrenheit;
+    }
+
+    public int converterKelvin() {
+        int kelvin = celsius + 273;
+        return kelvin;
+    }
+
+    public int getCelsius() {
+        return celsius;
+    }
+
+}
